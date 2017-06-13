4657e46b7e7fa7867f268a738d3ebc2f3a4c9049
Eric Bailey <eric@ericb.me>
2017-06-13 04:01:50 -0500

    hex2nix: 0.0.5 -> 0.0.6
    Also clean up whitespace and replace redundant installPhase with installFlags.

--- pkgs/development/tools/erlang/hex2nix/default.nix
+++ pkgs/development/tools/erlang/hex2nix/default.nix
@@ -1,23 +1,23 @@
-{stdenv, fetchFromGitHub, buildRebar3, ibrowse, jsx, erlware_commons, getopt }:
+{ stdenv, fetchFromGitHub, buildRebar3, ibrowse, jsx, erlware_commons, getopt }:
 
 buildRebar3 rec {
-    name = "hex2nix";
-    version = "0.0.5";
+  name = "hex2nix";
+  version = "0.0.6";
 
-    src = fetchFromGitHub {
-      owner = "erlang-nix";
-      repo = "hex2nix";
-      rev = "${version}";
-      sha256 = "07bk18nib4xms8q1i4sv53drvlyllm47map4c95669lsh0j08sax";
-    };
+  src = fetchFromGitHub {
+    owner = "erlang-nix";
+    repo = "hex2nix";
+    rev = "${version}";
+    sha256 = "17rkzg836v7z2xf0i5m8zqfvr23dbmw1bi3c83km92f9glwa1dbf";
+  };
 
-    beamDeps = [ ibrowse jsx erlware_commons getopt ];
+  beamDeps = [ ibrowse jsx erlware_commons getopt ];
 
-    DEBUG=1;
+  DEBUG = 1;
 
-    installPhase = ''
-      runHook preInstall
-      make PREFIX=$out install
-      runHook postInstall
-    '';
- }
+  installPhase = ''
+    runHook preInstall
+    make PREFIX=$out install
+    runHook postInstall
+  '';
+}
